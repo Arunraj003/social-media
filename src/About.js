c0487b4dff0f5b1b925c57a797c6b3c11bdae999
Arunraj003 <arunr6506@gmail.com>
2024-08-10 18:26:59 +0530

    commit "simple social media site"

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <main className='About'>
+      <h2>
+        About
+      </h2>
+      <p style={{ marginTop: "1rem"}}>Hi, I am Arun Raj. We Learn Together free for everyone.</p>
+    </main>
+  )
+}
+
+export default About
---
+++ src/Missing.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Missing = () => {
+  return (
+    <main className='Missing'>
+      <h1>Page Not Found</h1>
+      <p>Well, that's disappointed.</p>
+      <p>
+        Visit Our Homepage
+      </p>
+    </main>
+  )
+}
+
+export default Missing
